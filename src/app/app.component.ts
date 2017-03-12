07a3e025b9f11f8152e9120cf5de1038476adf42
NadimRai <rai_nadim@yahoo.com>
2017-03-11 22:39:59 -0800

    ability to edit meal name details calories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public foods: Meal[] = [
+  new Meal("Hamburger", "Didn't get a soda or cheese on my burger!", 354),
+  new Meal("Fries", "I only ate half of them.", 365)
+  ];
+  selectedMeal: Meal = null;
+  showDetails(clickedMeal: Meal) {
+    this.selectedMeal = clickedMeal;
+  }
+  finishedEditing() {
+  	this.selectedMeal = null;
+  }
 }
+export class Meal {
+	constructor(public name: string, public details: string, public calories: number) {}
+	}
